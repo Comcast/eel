e42f8628ac5edbc3a3a4f1a2001003973092e731
boriwo <mail@boriswolf.com>
2016-07-21 16:17:08 -0700

    removed some debug logging

--- eel/jtl/parser.go
+++ eel/jtl/parser.go
@@ -356,7 +356,7 @@ func (a *JExprItem) optimizeConditional(ctx Context, doc *JDoc) (*JExprItem, err
 			candB.mom = nil
 			candB.collapseIntoSingleNode(ctx, doc)
 			candB.adjustParameter()
-			ctx.Log().Debug("event", "eel_parser", "val_a", candA.val, "val_b", candB.val)
+			//ctx.Log().Debug("event", "eel_parser", "val_a", candA.val, "val_b", candB.val)
 			if candA.val == candB.val ||
 				(candA.val == true && candB.val == "true") ||
 				(candA.val == false && candB.val == "false") ||
